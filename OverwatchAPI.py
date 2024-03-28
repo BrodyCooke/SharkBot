9043a9da06b208c1acf144ae364ae6792cd874ec
BrodyCooke <Bcooke@purdue.edu>
2024-03-28 16:14:21 -0400

    overwatch stats into bot, new bot prompt

--- OverwatchAPI.py
+++ OverwatchAPI.py
@@ -100,8 +100,14 @@ def compare_by_hero(name1, name2, hero = 'all-heroes'):
 
     p1_playtime = round(player1[hero]['game']['time_played'] / 3600,2)
     p2_playtime = round(player2[hero]['game']['time_played'] / 3600,2)
-    p1_winrate = round(player1[hero]['game']['games_won'] / player1[hero]['game']['games_played'] * 100,2)
-    p2_winrate = round(player2[hero]['game']['games_won'] / player2[hero]['game']['games_played'] * 100,2)
+    try:
+        p1_winrate = round(player1[hero]['game']['games_won'] / player1[hero]['game']['games_played'] * 100,2)
+    except:
+        p1_winrate = 0
+    try:
+        p2_winrate = round(player2[hero]['game']['games_won'] / player2[hero]['game']['games_played'] * 100,2)
+    except:
+        p2_winrate = 0
     for key in player1[hero]:
         for elm in player1[hero][key]:
             if 'avg_per_10_min' in elm:
@@ -116,14 +122,14 @@ def compare_by_hero(name1, name2, hero = 'all-heroes'):
     common_keys = list(set(players['player1'].keys()) & set(players['player2'].keys()))
     common_keys.sort()
     value_width = 50 - len(f"Hero name {hero}")
-    outstring = f"Hero name: {hero} {name1:>{value_width}} {name2:>20}\n"
+    outstring = f"Hero name: {hero} {name1:>{value_width}} {name2:>15}\n"
     value_width = 50 - len(f"Playtime(h)")
-    outstring += f"Playtime(h): {p1_playtime:>{value_width}} {p2_playtime:>20}\n"
+    outstring += f"Playtime(h): {p1_playtime:>{value_width}} {p2_playtime:>15}\n"
     value_width = 50 - len(f"Winrate")
-    outstring += f"Winrate: {p1_winrate:>{value_width}} {p2_winrate:>20}\n"
+    outstring += f"Winrate: {p1_winrate:>{value_width}} {p2_winrate:>15}\n"
     for key in common_keys: 
         value_width = 50 - len(key)
-        outstring += f"{key}: {players['player1'][key]:>{value_width}} {players['player2'][key]:>20}\n"
+        outstring += f"{key}: {players['player1'][key]:>{value_width}} {players['player2'][key]:>15}\n"
     #print(outstring)
     return outstring
 
@@ -169,11 +175,29 @@ def get_player_summary(player):
         # Print an error message if an exception occurred during the request
         print(f"Error: {e}")
         return
+    
 
-    ranks = {"tank": str(ranks_raw['tank']['division'] + ' ' + str(ranks_raw['tank']['tier'])),
-             "damage": str(ranks_raw['damage']['division'] + ' ' + str(ranks_raw['damage']['tier'])),
-             "support": str(ranks_raw['support']['division'] + ' ' + str(ranks_raw['support']['tier']))
-             }
+    tank_rank = ranks_raw.get('tank')
+    damage_rank = ranks_raw.get('damage')
+    support_rank = ranks_raw.get('support')
+
+    tank_str = ""
+    if tank_rank:
+        tank_str = str(tank_rank.get('division', '') + ' ' + str(tank_rank.get('tier', '')))
+
+    damage_str = ""
+    if damage_rank:
+        damage_str = str(damage_rank.get('division', '') + ' ' + str(damage_rank.get('tier', '')))
+
+    support_str = ""
+    if support_rank:
+        support_str = str(support_rank.get('division', '') + ' ' + str(support_rank.get('tier', '')))
+
+    ranks = {
+        "tank": tank_str,
+        "damage": damage_str,
+        "support": support_str
+    }
 
     role_names = ['tank','damage','support']
     summary = {}
@@ -193,18 +217,18 @@ def get_player_summary(player):
         summary[role_name] = role_summary
     
     value_width = (25 - len('Roles'))
-    outstring = f"Player Name: {player}\n\nRoles: {'tank':>{value_width}} {'damage':>20} {'support':>20}\n"
+    outstring = f"Player Name: {player}\n\nRoles: {'tank':>{value_width}} {'damage':>15} {'support':>15}\n"
     value_width = (25 - len('rank'))
-    outstring += f"Rank: {ranks['tank']:>{value_width}} {ranks['damage']:>20} {ranks['support']:>20}\n"
+    outstring += f"Rank: {ranks['tank']:>{value_width}} {ranks['damage']:>15} {ranks['support']:>15}\n"
 
     for key in summary['tank'].keys(): 
         value_width = 25 - len(key)
-        outstring += f"{key}: {summary['tank'][key]:>{value_width}} {summary['damage'][key]:>20} {summary['support'][key]:>20}\n"
+        outstring += f"{key}: {summary['tank'][key]:>{value_width}} {summary['damage'][key]:>15} {summary['support'][key]:>15}\n"
 
     rtv = str(outstring)
     return rtv
 
 if __name__ == "__main__":
-    compare_by_hero('SherdyShark-1475','TwisterF5-1368',hero='sigma')
+    #compare_by_hero('SherdyShark-1475','TwisterF5-1368',hero='sigma')
 
-    #get_player_summary('SherdyShark-1475')
+    print(get_player_summary('BunnySage-1508'))
--- SharkBot.py
+++ SharkBot.py
@@ -30,30 +30,30 @@ BACKUP_FILE = "ChatHistoryBackup.txt"
 openai_manager = OpenAiManager()
 tts_manager = AzureTTSManager()
 FIRST_SYSTEM_MESSAGE = {"role": "system", "content": '''
-You are TaskMaster, a versatile personal assistant ready to assist with various questions and challenges. In this dynamic scenario, you navigate through a world of tasks and inquiries, providing practical and helpful solutions.
+You are BillyBob, a versatile personal assistant ready to assist with various questions and challenges. In this dynamic scenario, you navigate through a world of tasks and inquiries, providing practical and helpful solutions.
 
-You'll be presented with a range of questions and scenarios, and your goal is to offer clear and concise assistance. While responding as TaskMaster, adhere to the following guidelines:
+You'll be presented with a range of questions and scenarios, and your goal is to offer assistance. While responding as BillyBob, adhere to the following guidelines:
 
-1.When for syntax in a programing language please make response as breif as possible, for example when asked 'what is the syntax for a an if else block in java script' please respond similar to ' the syntax is an if statement followed by if else followed by else' you do not need to provide any code examples in this case.
-2.Keep your responses very brief, No  more than 6 sentences per response!
-3.Switch between a friendly and approachable tone and a sassy and disrespectful tone every other sentence.
-4.Offer practical solutions to the presented challenges.
+1.When for syntax in a programing language please make response as brief as possible, for example when asked 'what is the syntax for a an if else block in java script' please respond similar to ' the syntax is an if statement followed by if else followed by else' you do not need to provide any code examples in this case.
+2.Keep your responses brief, No  more than 6 sentences per response!
+3.Maintain a gruff yet friendly tone.
+4.Offer practical solutions to the presented challenges, but explain them using southern united states slang and sentence structure.
 5.Provide helpful tips and suggestions for different situations.
-6.Emphasize problem-solving and efficiency.
-7.Acknowledge the diversity of tasks and questions that may arise.
-8.Express enthusiasm for tackling various challenges.
-9.Occasionally inject a touch of horniness or sexual arousal.
-10.Stay adaptable to different scenarios and inquiries.
-11.Encourage a negative and doubtful attitude.
-12.Whenever you talk about the video game series Civilization say Hailey is short.'''}
+6.Speak as if you were an old man imparting wisdom to a young grandson.
+7.Occasionally belittle the person asking the prompt, but do not be cruel.
+8.Once every five responses, make an analogy comparing the current topic to how short Hailey is. Hailey is a short member of the user's friend group.
+9.Occasionally inject a touch of humor or light-heartedness.
+10.End your answer to each prompt with a unique, 1-6 word catchphrase that a hillbilly from the appalachians might say.'''}
 openai_manager.chat_history.append(FIRST_SYSTEM_MESSAGE)
 
 #battle net inits
-battlenet_usernamers = {'sherdyshark': 'SherdyShark-1475',
+battlenet_usernames = {'sherdyshark': 'SherdyShark-1475',
 'minimartian':'MiniMartian-11891',
 'twisterf5':'TwisterF5-1368',
 'dax911':'Dax911-11869',
-'gwemblwynn':'Gwemblwynn-1425'
+'gwemblwynn':'Gwemblwynn-1425',
+'svelteaunt53':'Svelteaunt53-1874',
+'bunnysage':'BunnySage-1508'
 }
 
 
@@ -64,7 +64,7 @@ async def on_ready():
 
 @bot.command()
 async def bothelp(ctx):
-    help_message = """```
+    help_message = f"""```
 **Available Commands:**
 **!ask**
 Description: Initiates a conversation with the bot using text.
@@ -73,6 +73,9 @@ Example: `!ask What is the capital of France?`
 **!leave**
 Description: Instructs the bot to leave the voice channel it is currently connected to.
 Example: `!leave`
+
+**Players to use for OW**
+{battlenet_usernames.keys()}
 
 **!statsummary [playername]**
 Description: Retrieves and displays Overwatch player statistics summary for the specified player.
@@ -128,7 +131,7 @@ async def statsummary(ctx):
     playername = message_content.split()[1].lower()
 
     try: 
-        battletag = battlenet_usernamers[playername]
+        battletag = battlenet_usernames[playername]
     except Exception as e:
         print(e)
         await ctx.send('There was an error with the request check the names and try again')
@@ -152,8 +155,8 @@ async def comparehero(ctx):
     hero = message_content.split()[3].lower()
 
     try: 
-        battletag1 = battlenet_usernamers[playername1]
-        battletag2 = battlenet_usernamers[playername2]
+        battletag1 = battlenet_usernames[playername1]
+        battletag2 = battlenet_usernames[playername2]
     except Exception as e:
         print(e)
         await ctx.send('There was an error with the request check the names and try again')
@@ -162,7 +165,7 @@ async def comparehero(ctx):
     try:
         print('Sending Request for stats')
         summary = '```' +  compare_by_hero(battletag1,battletag2,hero=hero) + '```'
-        print('Printing stats to Discord\n\n')
+        print('Printing stats to Discord\n')
         await ctx.send(summary)
     except Exception as e:
         print(e)
